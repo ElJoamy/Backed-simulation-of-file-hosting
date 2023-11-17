7eb6c35996e1ccde5e2857ead201b0ba3b3f6bc4
ElJoamy <josephsalguero1@upb.edu>
2023-11-17 14:17:48 -0400

    Refactor code for better readability and performance.

---
+++ src/infrastructure/api/controllers/controllers/apiRoutes.ts
@@ -0,0 +1,12 @@
+
+// const redisCacheService = new RedisCacheService();
+
+// const encrypt = new EncryptImpl();
+
+const API:string = '/api';
+
+export const routes = (server: any) => {
+    // server.use(`${API}/users`, userController.router);
+    // server.use(`${API}/roles`, roleController.router);
+    // server.use(`${API}/auth`, authController.router);
+};
---
+++ src/infrastructure/config/config.ts
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const env = {
+    port: process.env.PORT || 3000,
+    environment: process.env.ENV || 'develop'
+};
+console.log(env);
+
+export const db = {
+    port: process.env.DB_PORT || 3306,
+    type: process.env.DB_TYPE || 'mysql',
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS ||  'root',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'app',
+}
+console.log(db);
+
+export const lg = {
+    level: process.env.LOGGER_LEVEL || 'info'
+}
+
+export const jwt = {
+    secretKey:process.env.JWT_SECRET || 'your_secret_key',
+    expirationTime: process.env.JWT_EXPIRATION_TIME
+}
+console.log(jwt);
+
+export const redis_env = {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD || ''
+}
+console.log(redis_env);
